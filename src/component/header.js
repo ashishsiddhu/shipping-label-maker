5fa55192994f4002805abfd924c24d56ece04bb4
ashishsiddhu <32830765+ashishsiddhu@users.noreply.github.com>
2024-11-08 13:57:40 -0500

    Update header.js

--- src/component/header.js
+++ src/component/header.js
@@ -4,7 +4,7 @@ function Header() {
         <Navbar collapseOnSelect expand="lg" className="header-bg" variant="light">
             <Container>
                 <Navbar.Brand className="mx-auto py-3">
-                    <h3>Shipping Label Marker</h3>
+                    <h3>Shipping Label Marker...</h3>
                 </Navbar.Brand>
             </Container>
         </Navbar>
